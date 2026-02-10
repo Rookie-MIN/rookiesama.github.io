53c685bce38c3d9ee6dd82168aac8e3552fca54d
Rookie-MIN <2164067215@qq.com>
2026-02-10 21:21:17 +0800

    更新了博客

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -18,12 +18,32 @@ export type DeviceCategory = {
 export const devicesData: DeviceCategory = {
 	OnePlus: [
 		{
-			name: "OnePlus 13T",
-			image: "/images/device/oneplus13t.png",
-			specs: "Gray / 16G + 1TB",
+			name: "iQOO Neo9S Pro",
+			image: "https://www.bing.com/th/id/OIP.NF3Jd6DsUlA_n1rgByMRGAHaHa?w=170&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2",
+			specs: "Gray / 12G + 512GB",
 			description:
-				"Flagship performance, Hasselblad imaging, 80W SuperVOOC.",
-			link: "https://www.oneplus.com/cn/13t",
+				"天玑 9300+ 当年独挡",
+			link: "https://www.vivo.com.cn/vivo/iqooneo9spro/",
+		},
+	],
+	Computer: [
+		{
+			name: "隐星P15",
+			image: "https://www.colorful.com.cn/uploads/AttachFile/202305171636553782.jpg",
+			specs: "i5-12500H / 32G / 512G + 2T / 8G RTX 4060",
+			description:
+				"夯",
+			link: "https://www.colorful.com.cn/home/product?mid=158&id=33",
+		},
+	],
+	Tablet: [
+		{
+			name: "Xiaomi Pad 7 Pro",
+			image: "https://i02.appmifile.com/mi-com-product/fly-birds/xiaomi-pad-7-pro/pc/5484effe86e2b64e1bf9d0235e7bd126.jpg?f=webp",
+			specs: "12G + 256GB",
+			description:
+				"泡面盖子",
+			link: "https://www.mi.com/hk/product/xiaomi-pad-7-pro/",
 		},
 	],
 	Router: [
